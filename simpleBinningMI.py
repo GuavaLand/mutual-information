0e17d5456d168d9fb00789f119b91e5c31bcaef7
GuavaLand <yuan_yin@outlook.com>
2019-11-23 19:15:50 -0500

    troubleshoot simplebinning method and implement sparse matrix

--- simpleBinningMI.py
+++ simpleBinningMI.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import csr_matrix
 
 # Mutual Information on 2 Continuous Random Variables
 # Simple Binning:
@@ -13,30 +14,32 @@ import numpy as np
 
 
 
-def convert2IndicatorMatrix(variable, minVal, maxVal):
+def convert2IndicatorMatrix(variable, maxVal):
     '''Convert 1D np array variable (k instances) into k-by-bins indicator matrix'''
-    binBoundary = np.arange(minVal, maxVal+1) # increase max a bit so max won't be in a separate bin
-    pointBinID = np.digitize(variable,binBoundary)
-    binsOrder = np.arange(1,len(binBoundary)+1)
-    return (pointBinID[:,np.newaxis] == binsOrder).astype(int)
+    bins = np.arange(maxVal+1) # increase max a bit so max won't be in a separate bin
+    pointBinID = np.digitize(variable,bins) - 1 # convert to 0 based binID
+    return csr_matrix((pointBinID[:,np.newaxis] == bins).astype(int))
     
 def entropy(indicator_matrix, dim=1):
     '''Use indicator matrix of instance-by-bin to calculate MI.'''
     if dim==1:
-        probability = np.sum(indicator_matrix, axis = 0)/np.sum(indicator_matrix)
+        probability = indicator_matrix.sum(axis = 0)/indicator_matrix.sum()
+        logP = np.ma.log2(probability).filled(0)
     else:
-        probability = indicator_matrix/np.sum(indicator_matrix)
-    logP = np.ma.log2(probability).filled(0)
-    return np.sum(-probability * logP)
+        probability = indicator_matrix.data/indicator_matrix.sum()
+        logP = np.ma.log2(probability).filled(0)
+    return np.sum(np.multiply(-probability, logP))
 
 def MI(X,Y):
     '''Calculate mutual information of variables X and Y.'''
-    X = np.round(X-np.min(X))
-    Y = np.round(Y-np.min(Y))
+    X = np.round(X)
+    Y = np.round(Y)
     minVal = min(np.min(X),np.min(Y))
+    X = X-minVal
+    Y = Y-minVal
     maxVal = max(np.max(X),np.max(Y))
-    indicator_X = convert2IndicatorMatrix(X, minVal, maxVal)
-    indicator_Y = convert2IndicatorMatrix(Y, minVal, maxVal)
+    indicator_X = convert2IndicatorMatrix(X, maxVal)
+    indicator_Y = convert2IndicatorMatrix(Y, maxVal)
     indicator_XY = np.dot(indicator_X.T,indicator_Y)
     
     H_X = entropy(indicator_X)
@@ -44,6 +47,3 @@ def MI(X,Y):
     H_XY = entropy(indicator_XY, dim=2)
     mutualInfo = H_X + H_Y - H_XY
     return max(mutualInfo, 0)
-
-# TODO: sparse matrix
-# TODO: test cases
